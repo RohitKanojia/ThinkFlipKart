244ec590b12c46eda391ff6d2a5c03beb27ee638
Rohit <ee16b111@smail.iitm.ac.in>
2020-04-30 13:21:07 +0530

    New update

--- Code/second.java
+++ Code/second.java
@@ -1,5 +1,8 @@
 class second{
   public static void main(String args[]){
     System.out.println("Ding Dong");
+    for(int i = 0; i < 4; i++){
+    	System.out.println("ping pong" + i);
+    }	
   }
 }
